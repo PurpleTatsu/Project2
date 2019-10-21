092522cb17362e5438fc7e9e07fbb4388339ae3b
Katie <Tatsu@Katies-MacBook.local>
2019-10-21 17:17:54 -0400

    editing images and pushing to update

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,13 +5,16 @@ export default function Weather(props) {
   return (
     <div>
       <div id="instructions">
+        <img id="helper-image" src="https://github.com/PurpleTatsu/Project2/blob/master/src/components/images/weather-helper1.png?raw=true" />
         <h3>１・１枚ずつカードを見せながらリピート練習</h3>
+        <img id="weather-helper" src="https://github.com/PurpleTatsu/Project2/blob/master/src/components/images/weather-helper2.jpeg?raw=true" />
         <h3>２・♬ Weather song</h3>
         <p>絵を見せながらだったり、ダンスをしたりしながら歌うと楽しいです。</p>
         <h3>３・今日の天気を聞く　</h3>
+        <img id="helper-image" src="https://github.com/PurpleTatsu/Project2/blob/master/src/components/images/weather-helper3.jpeg?raw=true" />
         <p>練習を始めて間もない頃は、すべてのカードを並べて今日の天気をタッチさせてから、リピートしてもらってOK!
 </p>
-</div>
+      </div>
       <div id="flashcards">
         <img id="sunny" className="flashcard" src={require("/Users/Tatsu/SEI/project-2/src/components/images/sunny.png")} onClick={props.handleClick} alt="" />
         <img id="rainy" className="flashcard" src={require("/Users/Tatsu/SEI/project-2/src/components/images/rainy.png")} onClick={props.handleClick} alt="" />
